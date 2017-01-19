272909a948760afb4e451509bec0924cd6a8c224
Yoann <y.vernageau@gmail.com>
2017-01-19 17:34:26 +0100

    define project as ATL and fix module resolver

--- org.eclipse.m2m.atl.atlgt.ecore2km3/src/org/eclipse/m2m/atl/atlgt/ecore2km3/EcoreTransformationEmftvm.java
+++ org.eclipse.m2m.atl.atlgt.ecore2km3/src/org/eclipse/m2m/atl/atlgt/ecore2km3/EcoreTransformationEmftvm.java
@@ -1,5 +1,6 @@
 package org.eclipse.m2m.atl.atlgt.ecore2km3;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -14,6 +15,7 @@ import org.eclipse.m2m.atl.emftvm.impl.resource.EMFTVMResourceFactoryImpl;
 import org.eclipse.m2m.atl.emftvm.util.DefaultModuleResolver;
 import org.eclipse.m2m.atl.emftvm.util.ModuleResolver;
 import org.eclipse.m2m.atl.emftvm.util.TimingData;
+import org.osgi.framework.Bundle;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,16 +28,23 @@ public class EcoreTransformationEmftvm implements EcoreTransformation {
         ExecEnv env = EmftvmFactory.eINSTANCE.createExecEnv();
         ResourceSet resourceSet = new ResourceSetImpl();
 
+        Bundle bundle = Platform.getBundle("org.eclipse.m2m.atl.atlgt.ecore2km3");
+
+        // TODO Improve path resolver, string resolution is clearly not ideal
+        String resourcesPath = (bundle.getLocation() + "resources/").replace("reference:", "");
+
+        System.out.println(resourcesPath);
+
         final Metamodel ecoreMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
         ecoreMetamodel.setResource(EcorePackage.eINSTANCE.eResource());
 
         // TODO Link dynamically, according to the parameters given in the launcher
 //        Metamodel inMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
-//        inMetamodel.setResource(resourceSet.getResource(URI.createURI(EcoreTransformationEmftvm.class.getResource("./ClassDiagram/ClassDiagram.ecore").toString()), true));
+//        inMetamodel.setResource(resourceSet.getResource(URI.createURI(null), true)); // FIXME
 //        env.registerMetaModel("Ecore", inMetamodel);
 //
 //        Metamodel outMetamodel = EmftvmFactory.eINSTANCE.createMetamodel();
-//        outMetamodel.setResource(resourceSet.getResource(URI.createURI(EcoreTransformationEmftvm.class.getResource("./Relational/.Relational.ecore").toString()), true));
+//        outMetamodel.setResource(resourceSet.getResource(URI.createURI(null), true)); // FIXME
 //        env.registerMetaModel("KM3", outMetamodel);
 
         resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
@@ -50,10 +59,9 @@ public class EcoreTransformationEmftvm implements EcoreTransformation {
         outModel.setResource(resourceSet.createResource(URI.createURI(directory.getAbsolutePath() + metamodelPath.replace(".ecore", "-km3.ecore"))));
         env.registerOutputModel("OUT", outModel);
 
-        // TODO Where is the module ?
-        ModuleResolver mr = new DefaultModuleResolver("./", resourceSet);
+        ModuleResolver mr = new DefaultModuleResolver(resourcesPath, resourceSet);
         TimingData td = new TimingData();
-        env.loadModule(mr, "org.eclipse.m2m.atl.atlgt.ecore2km3");
+        env.loadModule(mr, "EMF2KM3");
         td.finishLoading();
         env.run(td);
         td.finish();
