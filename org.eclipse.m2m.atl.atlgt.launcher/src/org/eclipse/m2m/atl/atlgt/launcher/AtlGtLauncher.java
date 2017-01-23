ab17b27497830242f8e601b3bfaf2737c634c092
Massimo Tisi <massimo.tisi@mines-nantes.fr>
2017-01-23 16:39:19 +0100

    Comments and examples for all backward steps

--- org.eclipse.m2m.atl.atlgt.launcher/src/org/eclipse/m2m/atl/atlgt/launcher/AtlGtLauncher.java
+++ org.eclipse.m2m.atl.atlgt.launcher/src/org/eclipse/m2m/atl/atlgt/launcher/AtlGtLauncher.java
@@ -29,6 +29,48 @@ public class AtlGtLauncher implements ILaunchConfigurationDelegate {
         }
         else {
             throw new UnsupportedOperationException("Backward transformation is not supported yet");
+            
+			// D.1 XMI2DOT 
+			// 	String xmi2dot_commandline = atlgt.getPath() + "xmi2dot_generic_command"
+			// 		+ " -xmi " + targetUpdatedXMIFilePath e.g. test/myRelational.xmi
+			// 		+ " -dot " + targetUpdatedNormalDOTFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target-normal-updated.dot
+			// 		+ " -odot " + targetDOTNormalFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target-normal.dot
+			// 		+ " -km3 " + targetKm3Path e.g. test/myClassDiagram2myRelational-trace/Relational.km3
+			// 		+ " -pkg " + targetMetaModelName; e.g. Relational
+			// 	Runtime.getRuntime().exec(
+			// 	  xmi2dot_commandline
+			//     );
+
+			// D.2 Denormalization
+			// 	String bx_contract_commandline = groundtram.getPath() + "bx_contract -batch"
+			// 		+ " -src "  + targetDOTFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target.dot
+			// 		+ " -dst "  + targetUpdatedNormalDOTFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target-normal-updated.dot
+			// 	    + " -usrc " + targetUpdatedDOTFilePath; test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target-updated.dot
+			// 	Runtime.getRuntime().exec(
+			// 	  bx_contract_commandline						  
+			// 							  );
+
+			// E.1 Backward UnCAL
+			// 	String bwd_uncal_commandline = groundtram.getPath() + "bwd_uncal -t"
+			// 		+ " -db "  + sourceOriginalDOTFilePath e.g. test/myClassDiagram2myRelational-trace/myClassDiagram.dot
+			// 		+ " -udot "  + sourceUpdatedDOTFilePath e.g. test/myClassDiagram2myRelational-trace/myClassDiagram-updated.dot
+			// 		+ " -dot " + targetUpdatedDOTFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational-target-updated.dot
+			// 		+ " -xg "  + targetXGFilePath e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational.xg
+			// 		+ " -ei "  + targetEIFilePath; e.g. test/myClassDiagram2myRelational-trace/ClassDiagram2Relational.ei
+			// 	Runtime.getRuntime().exec(
+			// 							  bwd_uncal_commandline 
+			// 							  );
+
+			// F.1 DOT2XMI
+			// 	String dot2xmi_commandline = atlgt.getPath() + "dot2xmi_command"
+			// 		+ " -dot " + sourceUpdatedDOTFilePath e.g. test/myClassDiagram2myRelational-trace/myClassDiagram-updated.dot
+			// 		+ " -xmi " + sourceXMIFilePath e.g. test/myClassDiagram.xmi
+			// 		+ " -km3 " + sourceKm3Path e.g. test/myClassDiagram2myRelational-trace/ClassDiagram.km3
+			// 		+ " -pkg " + sourceMetaModelName; ClassDiagram
+			// 	Runtime.getRuntime().exec(
+			// 	  dot2xmi_commandline
+			// 							  );
+            
         }
 
         System.out.println("ATL-GT: Successfully executed");
