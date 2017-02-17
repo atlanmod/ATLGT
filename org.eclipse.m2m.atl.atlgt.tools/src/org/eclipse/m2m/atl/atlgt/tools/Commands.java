2099c8105fd1f7fbe3b7c0cff7f12ebfdbff8184
Yoann <y.vernageau@gmail.com>
2017-02-17 18:13:22 +0100

    fix the execution from a JAR bundle

--- org.eclipse.m2m.atl.atlgt.tools/src/org/eclipse/m2m/atl/atlgt/tools/Commands.java
+++ org.eclipse.m2m.atl.atlgt.tools/src/org/eclipse/m2m/atl/atlgt/tools/Commands.java
@@ -5,13 +5,23 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.Map;
+import java.util.Enumeration;
+import java.util.Objects;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import static java.util.Objects.isNull;
 
@@ -21,6 +31,11 @@ import static java.util.Objects.isNull;
 public class Commands {
 
     private static final String BUNDLE_SYMBOLIC_NAME = "org.eclipse.m2m.atl.atlgt.tools";
+
+    /**
+     * The temporary directory where to extract the executables.
+     */
+    private static Path tempDirectory;
 
     private Commands() {
         throw new IllegalStateException("This class should not be initialized");
@@ -55,21 +70,63 @@ public class Commands {
     private static Path resolve(String name) {
         IPath internalPath = new org.eclipse.core.runtime.Path(String.format("lib/%s/bin/%s", name, os()));
         Bundle bundle = Platform.getBundle(BUNDLE_SYMBOLIC_NAME);
-        Map<String, String> options = Collections.emptyMap();
 
         try {
-            URL url = FileLocator.resolve(FileLocator.find(bundle, internalPath, options));
+            URL url = FileLocator.resolve(bundle.getEntry("/"));
 
-            if (isNull(url)) {
-                throw new NullPointerException("Unable to find the binaries in '" + internalPath + "'");
+            // If the bundle is inside a JAR
+            if (Objects.equals(url.getProtocol(), "jar")) {
+                return extractJar(url).resolve(internalPath.toOSString());
             }
-
-            return Paths.get(url.toURI());
+            else {
+                return Paths.get(FileLocator.resolve(FileLocator.find(bundle, internalPath, Collections.emptyMap())).toURI());
+            }
         }
         catch (IOException | URISyntaxException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Extracts the content from a JAR file to a temporary directory.
+     *
+     * @param url the bundle URL
+     *
+     * @return the location
+     *
+     * @throws IOException if an I/O error occurs during the extraction
+     */
+    private static Path extractJar(URL url) throws IOException {
+        if (isNull(tempDirectory)) {
+            tempDirectory = Files.createTempDirectory("atlgt-tools");
+
+            String jarUrl = new URL(url.getPath()).getPath();
+            File jarFile = new File(jarUrl.substring(1, jarUrl.indexOf('!')));
+
+            System.out.println("Extracting binaries from: " + jarFile.getName());
+
+            // Extracting the content from the JAR
+            try (JarFile jar = new JarFile(jarFile)) {
+                for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements(); ) {
+                    JarEntry entry = enums.nextElement();
+
+                    Path fileName = tempDirectory.resolve(entry.getName());
+
+                    if (!entry.isDirectory() && entry.getName().startsWith("lib/") && entry.getName().contains(os())) {
+                        Files.createDirectories(fileName.getParent());
+
+                        try (InputStream in = new BufferedInputStream(jar.getInputStream(entry)); OutputStream out = new BufferedOutputStream(new FileOutputStream(fileName.toFile()))) {
+                            while (in.available() > 0) {
+                                out.write(in.read());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return tempDirectory;
     }
 
     private static String os() {
